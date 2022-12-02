f9ccc1bc745e9b6f9655abc6811abb52fe840b85
dancasnar1 <dancasnar1@alum.us.es>
2022-12-02 09:01:33 +0100

    Creation of backend
    Create project

---
+++ back/danejdaforum/src/main/java/com/danejdaforum/danejdaforum/DanejdaforumApplication.java
@@ -0,0 +1,13 @@
+package com.danejdaforum.danejdaforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanejdaforumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanejdaforumApplication.class, args);
+	}
+
+}
---
+++ back/danejdaforum/src/test/java/com/danejdaforum/danejdaforum/DanejdaforumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danejdaforum.danejdaforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanejdaforumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
